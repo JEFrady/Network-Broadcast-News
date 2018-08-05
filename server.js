dc39e86b4b54734a358c53ee4fdc759ea991f4de
JEFrady <jamie.e.halliday@gmail.com>
2018-08-04 19:52:31 -1000

    Can digest name, prints name when client disconnects

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ counter = 1;
 
 const server = net.createServer(client => {
   console.log("CLIENT CONNECTED!");
-  client.write("Welcome, what's your name? \n");
+  client.write("Welcome, use `User: [YourName]` to set your name \n");
   client.on("data", data => {
     let message = data.toString();
     console.log(message);
+    if (message.includes('User:')) {
+      client.name = message;
+    }
     clients.forEach(socket => {
-        socket.write(message);
-    })
 
+      if (client !== socket) {
+      socket.write(message);
+    }})
   });
+  client.on('end', () => {
+    console.log(client.name + ' disconnected');
+  })
   clients.push(client);
 });
 
